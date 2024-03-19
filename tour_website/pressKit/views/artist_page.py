0a785d25163de61c370c8d40ed584245d2d30377
Anthony <ajackerm@mtu.edu>
2024-03-19 18:15:05 -0400

    working

--- tour_website/pressKit/views/artist_page.py
+++ tour_website/pressKit/views/artist_page.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from ..models import videoLinks, socialLinks
+#from ..models import videoLinks, socialLinks
 
 def artist_page_view(request, artistName):
     # print(artistName)
