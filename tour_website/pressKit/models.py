c1df0e76dabfe9e58024350f68982b9001672600
Anthony <ajackerm@mtu.edu>
2024-03-19 18:53:05 -0400

    update

--- tour_website/pressKit/models.py
+++ tour_website/pressKit/models.py
@@ -18,29 +18,30 @@ class shows(models.Model):
     def __str__(self):
         return str(self.name)
     
-class artistInfo(models.Model):
-    name = models.CharField(max_length=50)
-    def __str__(self):
-        return str(self.name)
+# class artistInfo(models.Model):
+#     name = models.CharField(max_length=50)
+#     def __str__(self):
+#         return str(self.name)
     
 class videoLinks(models.Model):
-    artistChoice = models.ForeignKey(artistInfo, on_delete=models.CASCADE, blank=True, null=True)
+    artistName = models.CharField(max_length=50)
+    # artistChoice = models.ForeignKey(artistInfo, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=50)
     embedLink = models.CharField(max_length=500)
     def __str__(self):
         return str(self.artistChoice) + ' - ' + str(self.name)
     
-class socialLinks(models.Model):
-    artistChoice = models.ForeignKey(artistInfo, on_delete=models.CASCADE, blank=True, null=True)
-    instagram = models.CharField(max_length=300)
-    youtube = models.CharField(max_length=300)
-    spotify = models.CharField(max_length=300)
-    spotifyURI = models.CharField(max_length=300)
-    apple = models.CharField(max_length=300)
-    twitterX = models.CharField(max_length=300)
-    facebook = models.CharField(max_length=300)
-    tiktok = models.CharField(max_length=300)
-    snapchat = models.CharField(max_length=300)
-    soundcloud = models.CharField(max_length=300)
-    def __str__(self):
-        return str(self.artistChoice)
+# class socialLinks(models.Model):
+#     artistChoice = models.ForeignKey(artistInfo, on_delete=models.CASCADE, blank=True, null=True)
+#     instagram = models.CharField(max_length=300)
+#     youtube = models.CharField(max_length=300)
+#     spotify = models.CharField(max_length=300)
+#     spotifyURI = models.CharField(max_length=300)
+#     apple = models.CharField(max_length=300)
+#     twitterX = models.CharField(max_length=300)
+#     facebook = models.CharField(max_length=300)
+#     tiktok = models.CharField(max_length=300)
+#     snapchat = models.CharField(max_length=300)
+#     soundcloud = models.CharField(max_length=300)
+#     def __str__(self):
+#         return str(self.artistChoice)
