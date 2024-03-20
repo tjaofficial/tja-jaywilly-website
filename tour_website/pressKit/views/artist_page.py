a796ac3f325b83dcfa95e84cf144c68145367407
Anthony <ajackerm@mtu.edu>
2024-03-19 20:41:42 -0400

    Probelm finally fixed

--- tour_website/pressKit/views/artist_page.py
+++ tour_website/pressKit/views/artist_page.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
-#from ..models import videoLinks, socialLinks
+from ..models import videoLinks, socialLinks
 
 def artist_page_view(request, artistName):
-    # print(artistName)
-    # videoQuery = videoLinks.objects.all()
-    # socialQuery = socialLinks.objects.all()
-    # spotifyURI = False
-    # for artist in socialQuery:
-    #     if artist.artistChoice.name.lower() == artistName:
-    #         spotifyURI = artist.spotifyURI
+    print(artistName)
+    videoQuery = videoLinks.objects.all()
+    socialQuery = socialLinks.objects.all()
+    spotifyURI = False
+    for artist in socialQuery:
+        if artist.artistName.lower() == artistName:
+            spotifyURI = artist.spotifyURI
         
     
-    # videoList = []
-    # for video in videoQuery:
-    #     if video.artistChoice.name.lower() == artistName:
-    #         videoList.append(video)
+    videoList = []
+    for video in videoQuery:
+        if video.artistName.lower() == artistName:
+            videoList.append(video)
 
     return render(request, 'artist_page_template.html', {
         'header': True,
         'noFooter': True,
-        # 'videoList': videoList,
-        # 'spotifyURI': spotifyURI,
+        'videoList': videoList,
+        'spotifyURI': spotifyURI,
         'artistName': artistName
     })
 
